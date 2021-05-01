e34035e2c142f4a87a58cc51b4cb4a1efebba64b
Louis Quinnell <_@ziorobo.co>
2021-05-01 19:10:37 +1000

    Invoke the handler from the dev server

--- example/webpack.config.ts
+++ example/webpack.config.ts
@@ -1,8 +1,27 @@
+import { invoke } from "@zioroboco/bff"
 import { resolve } from "path"
 import HtmlWebpackPlugin from "html-webpack-plugin"
 import webpack from "webpack"
 
 const config: webpack.Configuration = {
+  devServer: {
+    before: (app, server, compiler) => {
+      app.all("/hello", async (req, res) => {
+        const handler = require("./bff/handler").handler
+        const bffResponse = await invoke({ handler, event: { path: req.path } })
+
+        res.statusCode = bffResponse.statusCode
+
+        res.write(
+          bffResponse.isBase64Encoded
+            ? Buffer.from(bffResponse.body, "base64")
+            : bffResponse.body
+        )
+
+        res.end()
+      })
+    },
+  },
   output: {
     path: resolve(__dirname, "build/app"),
   },
