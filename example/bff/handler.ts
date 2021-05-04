f04857a07065360c1e4124de46620938d0c2eb63
Louis Quinnell <_@ziorobo.co>
2021-05-04 19:46:55 +1000

    Include a network request in the example BFF

--- example/bff/handler.ts
+++ example/bff/handler.ts
@@ -1,7 +1,17 @@
+import { fetch } from "cross-fetch"
 import { setupBff } from "@zioroboco/bff/lib/runtime"
 
 export const { api, handler } = setupBff()
 
-api.get("/hello", (req, res) => {
-  return { message: "Hello world!" }
+api.get("/hello", async (req, res) => {
+  const { json: response } = await fetch("https://httpbin.org/anything", {
+    method: "POST",
+    body: JSON.stringify({ name: "world" }),
+  }).then(data => data.json())
+
+  return {
+    message: response?.name
+      ? `Hello ${response.name}!`
+      : "Something went wrong, but that's okay! ❤️",
+  }
 })
