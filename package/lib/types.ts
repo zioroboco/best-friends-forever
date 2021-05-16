830a035684e787679344e98e2ad81dcdd8d08f8c
Louis Quinnell <_@ziorobo.co>
2021-05-16 19:00:26 +1000

    Support api-gateway v2 payloads

--- package/lib/types.ts
+++ package/lib/types.ts
@@ -1,12 +1,17 @@
 import type {
   Context,
   APIGatewayProxyEvent as Event,
+  APIGatewayProxyEventV2 as EventV2,
   APIGatewayProxyResult as Result,
+  APIGatewayProxyResultV2 as ResultV2,
 } from "aws-lambda"
 
 export type { Context, Event, Result }
 
-export type Handler = (event: Event, context: Context) => Promise<Result>
+export type Handler = (
+  event: Event | EventV2,
+  context: Context
+) => Promise<Result | ResultV2>
 
 declare global {
   var BFF_SERVICE: string
