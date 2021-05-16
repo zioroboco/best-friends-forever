b0053dcab14fe3d532a81aecd97f56c5d1acd4f7
Louis Quinnell <_@ziorobo.co>
2021-05-17 00:28:09 +1000

    Miscellaneous dev-server TODOs

--- package/lib/dev-server.ts
+++ package/lib/dev-server.ts
@@ -13,7 +13,7 @@ const setupHandler = ({ handler, scenario }: Options): Handler => {
   const handlerModule = require(handler) as { init: any; handler: Handler }
   return scenario
     ? handlerModule.init({
-        // TODO: Get paths to scenarios from config
+        // TODO: Dev server needs to get paths to scenarios from config
         fetch: require(resolve(handler, "../../scenarios", scenario)).default(),
       })
     : handlerModule.handler
@@ -22,18 +22,21 @@ const setupHandler = ({ handler, scenario }: Options): Handler => {
 export function setupDevServer(options: Options): Configuration {
   let handler = setupHandler(options)
 
+  // TODO: Dev server is only watching the BFF module index
   watch(options.handler).on("all", (event, path) => {
     delete require.cache[require.resolve(options.handler)]
     handler = setupHandler(options)
   })
 
   return {
+    // TODO: Dev server middleware needs to be composable
     before: (app, server, compiler) => {
       app.all(endpoint("/*"), async (req, res) => {
         const bffResponse = await invoke({
           handler,
           event: {
             httpMethod: req.method,
+            // TODO: Remove duplicate uses of the BFF path prefix
             path: req.url.replace(`/bff/${BFF_SERVICE}`, ""),
             body: req.body,
           },
